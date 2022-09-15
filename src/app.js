c2f8cadde804b465037e712de3726c55e5ed7e11
Denver Jackson <denvergjackson@gmail.com>
2022-09-15 15:10:07 +0700

    Initial

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {hashHistory, Router, Route, Redirect} from 'react-router';
+import jQuery from "jquery";
+
+import CommentBox from "./components/comment-box";
+
+import Layout from './layout/layout';
+
+import BlogPage from './pages/blog';
+import PicturePage from './pages/picture';
+import VideoPage from './pages/video';
+
+const app = (
+	<Router history={hashHistory}>
+		<Redirect from="/" to="/blog" />
+		<Route path="/" component={Layout}>
+			<Route path="blog" component={BlogPage}/>
+			<Route path="picture" component={PicturePage}/>
+			<Route path="video" component={VideoPage}/>
+		</Route>
+	</Router>
+);
+
+jQuery(function() {
+  ReactDOM.render(
+    app,
+    document.getElementById('comment-box'),
+    function() {
+	    console.timeEnd('react-app');
+    }
+  );
+})
